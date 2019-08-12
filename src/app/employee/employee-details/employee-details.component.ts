5e2ac4f7bd4938c0149eeddcb4b8016998c000b6
sharath kulal <sharath_kulal@yahoo.com>
2019-08-12 17:13:24 -0400

    Read Route Parameters. snapshot approach

--- src/app/employee/employee-details/employee-details.component.ts
+++ src/app/employee/employee-details/employee-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { EmployeeService } from '../employee.service';
+import { Employee } from '../models/employee-model';
 
 @Component({
   selector: 'app-employee-details',
@@ -7,9 +10,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmployeeDetailsComponent implements OnInit {
 
-  constructor() { }
+  employee: Employee;
 
+  constructor(private _route: ActivatedRoute, private _employeeService: EmployeeService) { }
+
+  //Life cycle hooks
+  //+ is used to convert string to number
   ngOnInit() {
+    const id = +this._route.snapshot.params['id'];
+    this.employee =  this._employeeService.getEmployee(id);
   }
 
 }
